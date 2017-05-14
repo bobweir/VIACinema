0ac39542e22d2e86107ce9888ac1195f3a80c353
infer <infernoxe42@hotmail.fr>
2017-05-14 15:25:18 +0200

    Added web services (Payment from the web and movie list that i made (still not in use))

---
+++ App_Code/MovieList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for MovieList
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class MovieList : System.Web.Services.WebService
+{
+
+    public MovieList()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public List<String> ShowMovies()
+    {
+        List<String> movieList = new List<String>();
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\ASPNETDB.MDF;Integrated Security=True");
+        String sql = "SELECT Name FROM Movie";
+        try
+        {
+            
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            cmd.CommandType = CommandType.Text;
+            cmd.ExecuteNonQuery();
+            
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            da.Fill(table);
+
+            foreach(DataRow row in table.Rows)
+            {
+                movieList.Add(row.Field<String>(0));
+                
+            }
+
+        }
+        catch (System.Data.SqlClient.SqlException ex)
+        {
+            string error = "Error";
+            error += ex.Message;
+            throw new Exception(error);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return movieList;
+    }
+
+}
---
+++ Payement.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Payement.aspx.cs" Inherits="Payement" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
