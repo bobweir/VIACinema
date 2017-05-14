3d7a99dd2484187fc03bb11c7dc775c1ca7c5040
infer <infernoxe42@hotmail.fr>
2017-05-14 22:09:11 +0200

    Movie List with Web Services updated

---
+++ MovieList.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MovieList : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            this.BindGrid();
+        }
+    }
+
+    protected void BindGrid()
+    {
+        localhost.MovieList movieService = new localhost.MovieList();
+
+        GridView1.DataSource = movieService.ShowMovies();
+        GridView1.DataBind();
+
+    }
+}
